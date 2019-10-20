1b9e50369b3927505b743f5e69122240d105b19e
Tralljke <nvkzdotaplayer@gmail.com>
2019-10-20 23:47:33 +0700

    Добавьте файлы проекта.

---
+++ VisParam_Sniim/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace VisParam_Sniim
+{
+       public partial class MainWindow : Window
+    {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable VisParam;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // получаем строку подключения из app.config
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM Viscoelastic_param_table";
+            VisParam = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+                adapter.InsertCommand = new SqlCommand("InsertValue", connection);
+                connection.Open();
+                adapter.Fill(VisParam);
+                phonesGrid.ItemsSource = VisParam.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        private void UpdateDB()
+        {
+            SqlCommandBuilder comandbuilder = new SqlCommandBuilder(adapter);
+            adapter.Update(VisParam);
+            VisParam.Clear();
+            adapter.Fill(VisParam);
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateDB();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (phonesGrid.SelectedItems != null)
+            {
+                for (int i = 0; i < phonesGrid.SelectedItems.Count; i++)
+                {
+                    DataRowView datarowView = phonesGrid.SelectedItems[i] as DataRowView;
+                    if (datarowView != null)
+                    {
+                        DataRow dataRow = (DataRow)datarowView.Row;
+                        dataRow.Delete();
+                    }
+                }
+            }
+            UpdateDB();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win1 = new Window1();
+            win1.Show();
+        }
+    }
+}
+
---
+++ VisParam_Sniim/Window1.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace VisParam_Sniim
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        string connectionString;
+        
+
+        public Window1()
+        {
+           
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        }
+
+        private void Window_Loaded1(object sender, RoutedEventArgs e)
+        {
+       
+        }
+
+        private void Add1_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con1 = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("dbo.InsertValue", con1);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@radius", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@dielectric_constant", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@theoretical_polarization", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@measured_speed", TextBox4.Text);
+            cmd.Parameters.AddWithValue("@measured_polarization", TextBox5.Text);
+            cmd.Parameters.AddWithValue("@ID", "1");
+
+            con1.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+               
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                this.Close();
+                
+            }
+            con1.Close();
+
+        }
+
+        private void Add2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    
+}
