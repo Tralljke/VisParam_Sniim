0f2a30de3fdaa2542fbbffe8e98bf60aba8cf37d
Валерия Юрковлянец <v.yurkovlyanets@cdek.ru>
2020-06-06 16:07:14 +0700

    Ещё один класс статистической обработки + тест

---
+++ VisParam_Sniim/lognormalDistribution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisParam_Sniim
+{
+    class lognormalDistribution
+    {
+
+        public double[] GetlogNormal(double[] list)
+        {
+
+
+            return list;
+        }
+    }
+}
