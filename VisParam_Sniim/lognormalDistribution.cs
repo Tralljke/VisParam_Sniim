b0c3f78518529b752fb358dcd3ff4128b11db148
Валерия Юрковлянец <v.yurkovlyanets@cdek.ru>
2020-06-06 16:20:39 +0700

    New class and unit test for him

---
+++ VisParam_Sniim/lognormalDistribution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisParam_Sniim
+{
+    public class lognormalDistribution
+    {
+
+        public static double[] GetlogNormal(double[] list)
+        {
+            return list;
+        }
+    }
+}
